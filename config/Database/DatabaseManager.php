3962fd283d416e5ddb49b9666ebfa50722e8bb7b
GustGimenez <gustavogdd2@gmail.com>
2021-02-26 13:22:43 -0300

    Command loop

---
+++ config/Database/DatabaseManager.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace FormulaTG\Config\Database;
+
+
+class DatabaseManager
+{
+    public static function initialize(): void
+    {
+        $conn = Connection::createConnection();
+
+        $sql = 'DELETE FROM overtake';
+        $conn->exec($sql);
+
+        $sql = 'DELETE FROM competitor';
+        $conn->exec($sql);
+
+        $sql = 'DELETE FROM race';
+        $conn->exec($sql);
+
+        $sql = 'DELETE FROM car';
+        $conn->exec($sql);
+    }
+}
